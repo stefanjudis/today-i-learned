5bc8601378588afb505631b0c2c0ad76c486c42a
stefan judis <stefanjudis@gmail.com>
2018-08-27 17:06:59 +0200

    Fix post install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 # Contentful import should only run on the initial deploy to netlify button run
-if [[ -v CONTENTFUL_CMA_TOKEN ]] && [ -v NETLIFY_BUILD_BASE ]
+if [ -n "$CONTENTFUL_CMA_TOKEN" ] && [ -n "$NETLIFY_BUILD_BASE" ]
 then
   ./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
 fi
