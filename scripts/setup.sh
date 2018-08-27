736812313ff0579ba4592bf789b5e64dec347ecf
stefan judis <stefanjudis@gmail.com>
2018-08-27 15:45:56 +0200

    Fix post install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 printenv
 
-../node_modules/.bin/contentful space import --content-file ../contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
+./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
