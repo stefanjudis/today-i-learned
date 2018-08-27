c86f9cc83f327da17fb23d9f4a9686801ed3df1f
stefan judis <stefanjudis@gmail.com>
2018-08-27 16:51:19 +0200

    Improve netlify flow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
-printenv
-
-./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
+# Contentful import should only run on the initial deploy to netlify button run
+if [[ -v CONTENTFUL_CMA_TOKEN ]] && [ -v NETLIFY_BUILD_BASE ]
+then
+  ./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
+fi
--- src/lib/get-contentful-client.js
+++ src/lib/get-contentful-client.js
@@ -4,6 +4,7 @@ export default ({
   REACT_APP_CTF_SPACE: space,
   REACT_APP_CTF_CDA_TOKEN: accessToken,
   REACT_APP_CTF_CPA_TOKEN,
+  CONTENTFUL_CMA_TOKEN,
   NODE_ENV
 }) => {
   const config = {
@@ -14,6 +15,12 @@ export default ({
   if (NODE_ENV !== 'production') {
     config.accessToken = REACT_APP_CTF_CPA_TOKEN;
     config.host = 'preview.contentful.com';
+  }
+
+  if (CONTENTFUL_CMA_TOKEN) {
+    alert(
+      "You're done with the setup. 🎉🎉🎉 \nPlease remove `CONTENTFUL_CMA_TOKEN` from the Netlify build environment variables."
+    );
   }
 
   return contentful.createClient(config);
