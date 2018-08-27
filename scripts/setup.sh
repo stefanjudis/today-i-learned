2b7644b9e8c549d890f07e9af27bc6d16fc4b8b9
stefan judis <stefanjudis@gmail.com>
2018-08-27 18:50:10 +0200

    Fix cma key naming

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 # Contentful import should only run on the initial deploy to netlify button run
 if [ -n "$REACT_APP_CTF_CMA_TOKEN" ] && [ -n "$NETLIFY_BUILD_BASE" ]
 then
-  ./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $CONTENTFUL_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
+  ./node_modules/.bin/contentful space import --content-file ./contentful/export.json --management-token $REACT_APP_CTF_CMA_TOKEN --space-id $REACT_APP_CTF_SPACE
 else
   echo "I guess you're all set and can remove this script, the postinstall call and the contentful folder";
 fi
