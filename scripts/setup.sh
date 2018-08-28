075bad4e98bb180dfd2dfb055f1660879f465075
stefan judis <stefanjudis@gmail.com>
2018-08-28 11:01:50 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,11 @@
-# Contentful import should only run on the initial deploy to netlify button run
-# and also not in local environment (NETLIFY_BUILD_BASE won't be available locally)
+# To perform an import a CMA token has to be present
+# also the import shouldn't run when you set the project up
+# in your local dev environment (NETLIFY_BUILD_BASE won't be available locally)
 if [ -n "$REACT_APP_CTF_CMA_TOKEN" ] && [ -n "$NETLIFY_BUILD_BASE" ]
 then
-  TEST_ENTRY=4PZ4GqTqFWGg6qSaUCQsIS;
+  TEST_ENTRY_ID=4PZ4GqTqFWGg6qSaUCQsIS;
   RESPONSE_CODE=$(
-    curl "https://cdn.contentful.com/spaces/$REACT_APP_CTF_SPACE/environments/master/entries/$TEST_ENTRY?access_token=$REACT_APP_CTF_CDA_TOKEN" \
+    curl "https://cdn.contentful.com/spaces/$REACT_APP_CTF_SPACE/environments/master/entries/$TEST_ENTRY_ID?access_token=$REACT_APP_CTF_CDA_TOKEN" \
         --write-out %{http_code} \
         --request GET \
         --silent \
